b615b75203314e4c940fa9af82ec1317c1d891f9
AndresIngeniero2003 <restreporamirezandresfelipe@gmail.com>
2025-07-03 23:02:20 -0500

    avance

--- Entrenamientos/Semana3/gestion_api.js
+++ Entrenamientos/Semana3/gestion_api.js
@@ -1,4 +1,44 @@
-fetch('http://localhost:3000/productos')
-    .then( response => response.json() )
-    .then( data => console.log( "Productos disponibles: ", data ) )
-    .catch( error => console.error( "Error al obtener productos: ", error ) );
+/*La función es una solicitud asincrónica hecha con fetch() para obtener datos desde un servidor local que simula una API (como JSON Server). */
+
+fetch('http://localhost:3000/productos') //Hace una petición GET a la URL
+    .then( response => response.json() ) //Espera a que el servidor responda y convierte la respuesta en formato JSON
+    .then( data => console.log( "Productos disponibles: ", data ) ) //Muestra en la consola del navegador los productos obtenidos
+    .catch( error => console.error( "Error al obtener productos: ", error ) ); //Si ocurre un error lo muestra en la consola.
+
+/* usaremos fetch() con el método POST para agregar un nuevo producto a una base de datos simulada */ 
+const nuevoProducto = { id: 4, nombre: "Monitor", precio: 200 };
+/* Defino un objeto llamado nuevoProducto que representa un nuevo ítem a guardar en la lista de productos */
+
+fetch('http://localhost:3000/productos', {
+    method: 'POST', // Indica que se va a ENVIAR un nuevo recurso
+    headers: { 'Content-Type': 'application/json' }, // Especifica que el cuerpo del mensaje es JSON
+    body: JSON.stringify(nuevoProducto) // Convierte el objeto JS a una cadena JSON para enviarlo
+})
+/*Hace una solicitud HTTP POST al servidor falso en la ruta /productos para agregar el producto a la base de datos.*/
+
+    .then(response => response.json())
+    .then(data => console.log("Producto agregado:", data))
+    .catch(error => console.error("Error al agregar producto:". error));
+
+
+//Actualización de Datos (PUT): modifica las propiedades de un producto existente. 
+const productoActualizado = {id: 1, nombre: "Laptop", precio: 1400 };
+
+fetch('http://localhost:3000/productos/1', {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(productoActualizado)
+})
+
+    .then(response => response.json())
+    .then(data => console.log("Producto actualizado:", data))
+    .catch(error => console.error("Error al actualizar producto:", error));
+
+
+//  elimina un producto del servidor basándote en su ID
+fetch ('http://localhost:3000/productos/3', {
+    method: DELETE})
+    .then(() => console.log("Producto eliminado"))
+    .catch(error => console.error("Error al eliminar producto:", error));
+
+
